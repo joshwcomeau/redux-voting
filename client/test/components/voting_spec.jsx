56dd4bbed128e3ae0119bd936fb719a9f6edf424
Joshua Comeau <joshwcomeau@gmail.com>
2015-12-11 18:43:15 -0500

    Introduced Redux to the front-end. Rejigged components to follow its pattern.

--- client/test/components/voting_spec.jsx
+++ client/test/components/voting_spec.jsx
@@ -4,7 +4,7 @@ import {
   renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate
 } from 'react-addons-test-utils'
 
-import Voting from '../../src/components/Voting';
+import {Voting} from '../../src/components/Voting';
 
 describe('Voting', () => {
 
